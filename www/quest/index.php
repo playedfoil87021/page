8be012835625bd6a080efa07a9122813f14a7527
playedfoil87021 <atan.mitan.nitan@gmail.com>
2023-12-06 11:20:57 +0900

    test

--- www/quest/index.php
+++ www/quest/index.php
@@ -18,6 +18,7 @@ session_start();
 function getRandomQuestFromDatabase($connInFunc)
 {
     $rowCount = 'SELECT count(*) FROM questwalker.quest_list';
+    echo "<p>$rowCount</p>";
     $query = "SELECT qu_name FROM questwalker.quest_list ORDER BY RAND() LIMIT 1";
     $result = mysqli_query($connInFunc, $query);
 
