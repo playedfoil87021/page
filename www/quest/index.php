ec2cdb261b75c67c5f3c733c77dc58243f158e3a
playedfoil87021 <atan.mitan.nitan@gmail.com>
2023-12-12 10:20:39 +0900

    2023/12/12 10:20:38.99 edit by 204074

--- www/quest/index.php
+++ www/quest/index.php
@@ -29,7 +29,7 @@ function getRandomQuestFromDatabase($connInFunc)
         // クエリの実行に失敗した場合、エラー処理を行う代わりに false を返す
         return false;
     }
-    $getTime = mysqli_query($connInFunc,"SELECT time FROM questwalker.quest_list LIMIT $randomNum,1");
+    $getTime = mysqli_query($connInFunc,"SELECT timer FROM questwalker.quest_list LIMIT $randomNum,1");
     questDataSet($randomNum,$getTime);
 
     $row = mysqli_fetch_assoc($result);
