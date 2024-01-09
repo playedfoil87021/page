508ec512dd4a1d3183542c0a328ab72f77d67321
akanekozzzm <yuto.hill@gmail.com>
2024-01-09 11:59:52 +0900

    4

--- www/quest/index.php
+++ www/quest/index.php
@@ -1,6 +1,18 @@
 <?php
 //DB連携
+// MySQLサーバーへの接続パラメータの設定
+$servername = "localhost:65233";
+$username = "root";
+$password = "P2L13foJQeebl3Jl";
+$dbname = "questwalker";
 
+// MySQLデータベースに接続
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// 接続エラーの確認
+if (!$conn) {
+    die("データベースへの接続に失敗しました: " . mysqli_connect_error());
+}
 //クエストランダム
 
 //クエスト変更（制限）
@@ -23,9 +35,10 @@
 
     <p>終了時間</P>
 
-    <p>達成報告</p>
-
-    <p>あきらめる</p>
+    <div class="btn">
+        <a href=./report><button class="success">達成報告</button></a>
+        <button class="forgo">あきらめる</button>
+    </div>
 
 </body>
 </html>
