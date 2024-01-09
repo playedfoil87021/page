4b6061b628b9d5757a1cbb0cd031d1bb4388679d
akanekozzzm <yuto.hill@gmail.com>
2024-01-09 11:35:00 +0900

    1

--- www/quest/index.php
+++ www/quest/index.php
@@ -65,8 +65,13 @@ function questDataSet($questId, $timeLimit)
 
 <html>
 <head>
-
+<meta charset="UTF-8">
+    <title>クエストトップ</title>
+    <link rel="stylesheet" href="top.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="top.js"></script>
 </head>
+<header><iframe src="../header/header1.php" frameborder="0" width="100%" scrolling="no"></iframe></header>
 <body>
 
 </body>
