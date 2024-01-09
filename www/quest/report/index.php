cd4114437a29a4226137b5b6439dbf245d738db5
akanekozzzm <yuto.hill@gmail.com>
2024-01-09 11:00:48 +0900

    yas

--- www/quest/report/index.php
+++ www/quest/report/index.php
@@ -1,3 +1,19 @@
+<?php
+// MySQLサーバーへの接続パラメータの設定
+$servername = "localhost:65233";
+$username = "root";
+$password = "P2L13foJQeebl3Jl";
+$dbname = "questwalker";
+
+// MySQLデータベースに接続
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// 接続エラーの確認
+if (!$conn) {
+    die("データベースへの接続に失敗しました: " . mysqli_connect_error());
+}
+mission_tracker
+?>
 <html>
 
 <head>
@@ -8,20 +24,13 @@
 </head>
 <header><iframe src="../../header/header1.php" frameborder="0" width="100%" scrolling="no"></iframe></header>
 
-<nav>
-    <ul>
-        <li><a href="../top/index.php">トップ</a></li>
-        <li><a href="../recruit/RecuQuest.html">クエスト履歴</a></li>
-        <li><a href="../history/index.html">クエスト受注履歴</a></li>
-        <li><a href="../achievement/">アチーブメント</a></li>
-    </ul>
-</nav>
-
 <body>
     <div class="quest_area">
         <div class="title">現在受けれるクエストはこちら！</div>
         <div class="box_area">
-            <div class="quest_title inbox">ここにクエスト名</div>
+            <div class="quest_title inbox">
+           
+            </div>
             <div class="quest_time inbox">目標時間:</div>
         </div>
 
