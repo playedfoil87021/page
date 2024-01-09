9c1cf3a4a6f4503c2ceeb34b26dc60a014aa2591
irawrath <thisyuuki@yahoo.co.jp>
2024-01-09 11:26:32 +0900

    2024/01/09 11:26:31.67 edit by 204005

--- www/quest/index.php
+++ www/quest/index.php
@@ -3,6 +3,32 @@ $servername = "localhost:65233";
 $username = "root";
 $password = "P2L13foJQeebl3Jl";
 $dbname = "questwalker";
+
+session_start();
+function getRandomQuestFromDatabase($connInFunc)
+{
+    //クエストの行数を取得
+    $rowCount = mysqli_num_rows(mysqli_query($connInFunc, 'SELECT * FROM questwalker.quest_list'));
+    //乱数をもとにクエストを決定
+    $randomNum = rand(0, $rowCount - 1);
+
+    //クエストを渡す
+    $query = "SELECT qu_name FROM questwalker.quest_list LIMIT $randomNum,1";
+    $result = mysqli_query($connInFunc, $query);
+
+    if (!$result) {
+        // クエリの実行に失敗した場合、エラー処理を行う代わりに false を返す
+        return false;
+    }
+    $getTimeResult = mysqli_query($connInFunc, "SELECT timer FROM questwalker.quest_list LIMIT $randomNum,1");
+    $timeLimitRow = mysqli_fetch_assoc($getTimeResult);
+    $timeLimit = $timeLimitRow['timer'];
+    questDataSet($randomNum, $timeLimit);
+
+    $row = mysqli_fetch_assoc($result);
+
+    return $row['qu_name'];
+}
 function questDataSet($questId, $timeLimit)
 {
     $dsn = 'mysql:dbname=questwalker;host=localhost:65233;charset=utf8';
