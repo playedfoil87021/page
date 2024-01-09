e61ac739ec26e44dc00eea13587fc584d2ddb151
akanekozzzm <yuto.hill@gmail.com>
2024-01-09 12:02:16 +0900

    5

--- www/quest/index.php
+++ www/quest/index.php
@@ -49,19 +49,19 @@ function questDataSet($questId, $timeLimit)
     $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $usrId = $_SESSION['accountNumId'];
     $questEnd = date("Y/m/d H:i:s", strtotime("$timeLimit sec"));
-    //$changeCountInFunc = $_SESSION['change_count'];
-
     try {
-        $sql = "INSERT INTO mission_tracker(user_id,quest_id,quest_end,change_count) VALUES (:user_id,:quest_id,:quest_end,:change_count)on duplicate key update quest_id = VALUES(quest_id), quest_end = VALUES(quest_end),change_count = VALUES(change_count);";
+        $sql = "INSERT INTO mission_tracker(
+                    user_id,quest_id,quest_end,change_count
+                ) VALUES (
+                    :user_id,:quest_id,:quest_end,:change_count
+                )on duplicate key update 
+                    quest_id = VALUES(quest_id), 
+                    quest_end = VALUES(quest_end),
+                    change_count = VALUES(change_count);";
         $stmt = $PDO->prepare($sql);
         $stmt->bindParam(':user_id', $usrId);
         $stmt->bindParam(':quest_id', $questId);
-        $stmt->bindParam(':quest_end', $questEnd);
-        $stmt->bindParam(':change_count', $changeCountInFunc);
-        $stmt->execute();
-    } catch (PDOException $e) {
-        // エラーハンドリング：エラーが発生した場合にエラーメッセージを表示
-        exit('データベースエラー: ' . $e->getMessage());
+
     }
 
 }
